790fe3bb217b66224ec09b8fdee7915e1439d693
joyadomari-chun24 <74032103+joyadomari-chun24@users.noreply.github.com>
2023-01-14 14:23:30 -1000

    Updated to be cooler

--- teamcode/MecanumDriveTeleOP1.java
+++ teamcode/MecanumDriveTeleOP1.java
@@ -19,6 +19,8 @@ public class MecanumDriveTeleOP1 extends LinearOpMode {
   private DcMotorEx DriveBR;
   private DcMotorEx DriveLS;
   private Servo Claw;
+  double servoOpen = 1;
+  double servoClose = 0.1;
 
   /**
    * This function is executed when this Op Mode is selected from the Driver Station.
@@ -37,12 +39,12 @@ public class MecanumDriveTeleOP1 extends LinearOpMode {
     DriveLS = hardwareMap.get(DcMotorEx.class, "DriveLS");
     Claw = hardwareMap.get(Servo.class, "Claw");
 
-    Claw.setPosition(0.79);
+    Claw.setPosition(servoClose);
 
-    /*DriveFL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    DriveFL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     DriveFR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     DriveBL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-    DriveBR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);*/
+    DriveBR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
     waitForStart();
     if (opModeIsActive()) {
@@ -60,29 +62,28 @@ public class MecanumDriveTeleOP1 extends LinearOpMode {
           DriveFR.setPower((((Forward - Slide) - Turn) / Limiter) / 4);
           DriveBR.setPower((((Forward + Slide) - Turn) / Limiter) / 4);
         } else if (gamepad1.left_bumper) {
-          DriveFL.setPower(((Forward + Slide + Turn) / Limiter));
-          DriveBL.setPower((((Forward - Slide) + Turn) / Limiter));
-          DriveFR.setPower((((Forward - Slide) - Turn) / Limiter));
-          DriveBR.setPower((((Forward + Slide) - Turn) / Limiter));
-        } else {
           DriveFL.setPower(((Forward + Slide + Turn) / Limiter) / 2);
           DriveBL.setPower((((Forward - Slide) + Turn) / Limiter) / 2);
           DriveFR.setPower((((Forward - Slide) - Turn) / Limiter) / 2);
           DriveBR.setPower((((Forward + Slide) - Turn) / Limiter) / 2);
+        } else {
+          DriveFL.setPower(((Forward + Slide + Turn) / Limiter));
+          DriveBL.setPower((((Forward - Slide) + Turn) / Limiter));
+          DriveFR.setPower((((Forward - Slide) - Turn) / Limiter));
+          DriveBR.setPower((((Forward + Slide) - Turn) / Limiter));
           /*
            * Fl -1, forwards; updated, forwards
            * BL -1, backwards; updated, forwards
            * FR -1, backwards; updated, forwards
            * BR -1, forwards; updated, forwards
            * on else statement: up/down works, left/right works, turn works
-           * what does limiter do?
            * */
         }
 
         if (gamepad2.a) {
-          Claw.setPosition(0.79);
+          Claw.setPosition(servoOpen);
         } else if (gamepad2.b) {
-          Claw.setPosition(0.5);
+          Claw.setPosition(servoClose);
         }
 
         if (gamepad1.a) {
@@ -90,7 +91,29 @@ public class MecanumDriveTeleOP1 extends LinearOpMode {
           DriveFL.setPower(1);
           DriveBR.setPower(-1);
           DriveFR.setPower(-1);
-          sleep(1100);
+          sleep(650);
+          DriveBL.setPower(0);
+          DriveFL.setPower(0);
+          DriveBR.setPower(0);
+          DriveFR.setPower(0);
+        }
+        if (gamepad1.b) {
+          DriveBL.setPower(1);
+          DriveFL.setPower(1);
+          DriveBR.setPower(-1);
+          DriveFR.setPower(-1);
+          sleep(325);
+          DriveBL.setPower(0);
+          DriveFL.setPower(0);
+          DriveBR.setPower(0);
+          DriveFR.setPower(0);
+        }
+        if (gamepad1.x) {
+          DriveBL.setPower(-1);
+          DriveFL.setPower(-1);
+          DriveBR.setPower(1);
+          DriveFR.setPower(1);
+          sleep(325);
           DriveBL.setPower(0);
           DriveFL.setPower(0);
           DriveBR.setPower(0);
@@ -102,13 +125,19 @@ public class MecanumDriveTeleOP1 extends LinearOpMode {
          */
         if (DriveLS.getCurrentPosition() >= -150)
           DriveLS.setPower(-1 * gamepad2.right_trigger * 0.95);
-        else if (DriveLS.getCurrentPosition() <= -6200)
+        else if (DriveLS.getCurrentPosition() <= -6100)
           DriveLS.setPower(gamepad2.left_trigger * 0.95);
         else
           DriveLS.setPower(gamepad2.left_trigger * 0.95 - gamepad2.right_trigger * 0.95); //Left, up; right, down
 
-        /* if(gamepad2.y)
-          DriveLS.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); */
+        if(gamepad2.y)
+        {
+          DriveLS.setPower(0.5);
+          sleep(400);
+          DriveLS.setPower(0);
+
+          DriveLS.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        }
 
         telemetry.addData("FR Angular Velocity: ", DriveFR.getVelocity(AngleUnit.RADIANS));
         telemetry.addData("FL Angular Velocity: ", DriveFL.getVelocity(AngleUnit.RADIANS));
@@ -121,6 +150,7 @@ public class MecanumDriveTeleOP1 extends LinearOpMode {
         telemetry.addData("Left Stick X: ", gamepad1.left_stick_x);
         telemetry.addData("Left Stick Y: ", gamepad1.left_stick_y);
         telemetry.addData("Claw Position: ", Claw.getPosition());
+        telemetry.addData("Claw Target Open/Close: ", gamepad1.a);
         telemetry.addData("LS Position: ", DriveLS.getCurrentPosition());
         telemetry.update();
       }
